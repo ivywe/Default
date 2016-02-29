f37127c563fde17deeace19d00077a4e18a3c810
Ivy <komma@ivywe.co.jp>
2016-02-29 11:10:16 +0900

    remove javascript beforeunload()

--- default/javascript/theme.js
+++ default/javascript/theme.js
@@ -1,11 +1,1 @@
-
-$(function(){
-    $("input[ type=text ]").change(function() {
-        $(window).on('beforeunload', function() {
-            return 'Form is not completed. Are you leave?';
-        });
-    });
-    $("input[ type=submit ]").click(function() {
-        $(window).off('beforeunload');
-    });
-});
+ 
